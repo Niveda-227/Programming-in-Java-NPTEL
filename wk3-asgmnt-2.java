0b072490bff5a36a2ca5d92c7ad28e81771e7e0b
Niveda J <83544318+Niveda-227@users.noreply.github.com>
2022-02-23 22:09:02 +0530

    Create wk3-asgmnt-2.java

---
+++ wk3-asgmnt-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Circle extends Point{
+	
+public static void main(String[] args) {
+
+	 Scanner sc = new Scanner(System.in);
+	 Point p1=new Point();
+	 p1.x=sc.nextDouble();
+	 p1.y=sc.nextDouble();
+	 Point p2=new Point();
+	 p2.x=sc.nextDouble();
+	 p2.y=sc.nextDouble(); 
+	 Circle c1=new Circle();
+	c1.distance(p1,p2);
+	
+  }
+
+}
+
+class Point{
+  double x;
+  double y;
+
+public static void distance(Point p1,Point p2){
+        double d;
+	  d=Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+	  System.out.println(d);
+  }	
+}  
